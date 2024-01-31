a18a5ac3afe555e60ec8d1b19ad3454373b4ac4c
maximilien_notz <maximiliennotz@gmail.com>
2024-01-30 21:36:04 -0700

    Code clean up and java doc

---
+++ src/Stack.java
@@ -0,0 +1,46 @@
+/**
+ * @author max
+ */
+public class Stack <Type>
+{
+    class Node <Type>
+    {
+        public Type data;
+        public Node nextNode;
+
+        public Node(Type data)
+        {
+            this.data = data;
+            this.nextNode = null;
+        }
+
+        public Node(Type data, Node nextNode)
+        {
+            this.data = data;
+            this.nextNode = nextNode;
+        }
+    }
+    private Node head;
+
+    public Stack()
+    {
+        this.head = null;
+    }
+    
+    public void push(Type data)
+    {
+        this.head = new Node(data, this.head);
+    }
+    
+    public Type pop()
+    {
+        Type temp = (Type)head.data;
+        this.head = head.nextNode;
+        return (Type)temp;
+    }
+    
+    public boolean isEmpty()
+    {
+        return this.head == null;
+    }
+}
